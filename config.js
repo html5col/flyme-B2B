5ffe19988a81a4b45b69a99659778e2d0555df5e
FrankLee <frank25184@sina.com>
2019-09-27 18:38:35 +0800

    set admin

--- config.js
+++ config.js
@@ -92,7 +92,7 @@ var config = {
   weibo_id: 'your_weibo_id',
 
   // admin 可删除话题，编辑标签。把 user_login_name 换成你的登录名
-  admins: { enEnthusiast: true },
+  admins: { enthusiast: true },
 
   //github 登陆的配置
   GITHUB_OAUTH: {
